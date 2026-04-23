60f8f74c12fb98e85163c89256aa90f6dddda6fa
Yeerhanti <y_bahetikailaide@kbtu.kz>
2026-04-23 16:41:51 +0500

    Add Practice1 Python basics examples

---
+++ Practice 01/python-basics/data_types.py.py
@@ -0,0 +1,42 @@
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
