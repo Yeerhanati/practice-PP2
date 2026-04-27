bc19e6fb994c7d0654f037e2590664b34b3aeb0e
Yeerhanti <y_bahetikailaide@kbtu.kz>
2026-04-27 16:16:19 +0500

    Add Practice1 Python basics examples

---
+++ Practice 01/python-basics/strings.py
@@ -0,0 +1,24 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
